490b16340b7ef61d2b56fd14c7cd8d8d3479a47b
Indrek Ots <ots.indrek@gmail.com>
2016-06-30 19:07:33 +0300

    implement chapter 3 exercises

---
+++ 03_functions/bean-counting.js
@@ -0,0 +1,12 @@
+function countBs(str) {
+  return countChar(str, "B");
+}
+
+function countChar(str, char) {
+  var count = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) == char)
+      count++;
+  }
+  return count;
+}
---
+++ 03_functions/recursion.js
@@ -0,0 +1,12 @@
+function isEven(x) {
+  x = Math.abs(x);
+  if (x == 0) {
+    return true;
+  }
+  else if (x == 1) {
+    return false;
+  }
+  else {
+    return isEven(x - 2);
+  }
+}
